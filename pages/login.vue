19d949704a273c714ffbb6367d5f4860b0f34db0
Falk0r <g.falkor@gmail.com>
2021-01-16 19:56:29 +0100

    add test route

--- pages/login.vue
+++ pages/login.vue
@@ -2,14 +2,17 @@
   <div class="container">
     <h1>Sign in to access the secret page</h1>
     <div>
+      <label for="name">
+        <input id="name" type="name" value="test" v-model="name" placeholder="name">
+      </label>
       <label for="email">
-        <input id="email" type="email" value="test">
+        <input id="email" type="email" value="test" v-model="email" placeholder="email">
       </label>
       <label for="password">
-        <input id="password" type="password" value="test">
+        <input id="password" type="password" value="test" v-model="password" placeholder="password">
       </label>
       <button @click="postLogin">
-        login
+        register
       </button>
       <p>The credentials are not verified for the example purpose.</p>
     </div>
@@ -21,16 +24,38 @@ const Cookie = process.client ? require('js-cookie') : undefined
 
 export default {
   middleware: 'notAuthenticated',
+  data() {
+      return {
+          name: null,
+          email: null,
+          password: null
+      }
+  },
   methods: {
     postLogin () {
-      setTimeout(() => { // we simulate the async request with timeout.
-        const auth = {
-          accessToken: 'someStringGotFromApiServiceWithAjax'
+        const user = {
+            'name': this.name,
+            'email': this.email,
+            'password': this.password
         }
-        this.$store.commit('setAuth', auth) // mutating to store for client rendering
-        Cookie.set('auth', auth) // saving token in cookie for server rendering
-        this.$router.push('/dashboard')
-      }, 1000)
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        fetch('http://localhost:5000/test', options)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res);
+                const auth = {
+                accessToken: 'someStringGotFromApiServiceWithAjax'
+                }
+                this.$store.commit('setAuth', auth) // mutating to store for client rendering
+                Cookie.set('auth', auth) // saving token in cookie for server rendering
+                this.$router.push('/dashboard')
+            })
     }
   }
 }
